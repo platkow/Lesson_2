73ce07eb9d3ca66f57c72cfb371c3524c074b9c8
Paulina <platkowska@sii.pl>
2021-04-27 09:34:08 +0200

    Solution.java update. Token_TestData.txt provided.

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,46 +9,75 @@ public class Solution {
     public static void main(String[] args) {
 
         String token = Solution.tokenGenerator();
-        System.out.println("Your unique token is: " + token);
-
+        if (!(token.isEmpty())) {
+            System.out.println("Your unique token is: " + token);
+        } else {
+            System.out.println("Token cannot be generated due to invalid user input value.");
+        }
     }
 
     private static String tokenGenerator() {
-        String lowLetters = "abcdefghijklmnopqrstuvwxyz";
-        String bigLetters = lowLetters.toUpperCase();
-        String numbers = "123456789";
-        String specialSigns = "!@#$%^&*()";
-
         Random randomNumber = new Random();
 
-        //build token range
-        StringBuilder tokenRange = new StringBuilder();
-        tokenRange.append(lowLetters)
-                .append(bigLetters)
-                .append(numbers)
-                .append(specialSigns);
+        //Read token range from file
+        String path = "C:\\Users\\" + System.getProperty("user.name") + "\\Desktop\\Token_TestData.txt";
+        String dataRead;
+        ArrayList<String> tokenDataSetCollection = new ArrayList<>();
 
-        String tokenRangeString = tokenRange.toString();
-
-        //token length user input
-        System.out.println("Please enter value 5, 10 or 15 to generate token with corresponding length.");
-
-        Scanner scanner = new Scanner(System.in);
-        int userTokenLength = scanner.nextInt();
-
-        //Creating token from single characters. Token length dependent on user input.
-        StringBuilder generatedToken = new StringBuilder();
-
-        for (int i = 0; i < userTokenLength; i++) {
-            String singleCharacter = String.valueOf(tokenRangeString.charAt(randomNumber.nextInt(tokenRangeString.length())));
-            generatedToken.append(singleCharacter);
+        try {
+            File tokenDataSet = new File(path);
+            Scanner dataSet = new Scanner(tokenDataSet);
+            while (dataSet.hasNext()) {
+                dataRead = dataSet.nextLine();
+                tokenDataSetCollection.add(dataRead);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
         }
 
-        String generatedTokenString = generatedToken.toString();
+        //token length user input
+        System.out.println("Please enter integer 5, 10 or 15 to generate token with corresponding length.");
 
+        int userTokenLength = 0;
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            userTokenLength = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid data type provided.");
+        }
+
+        //Creating token based on collection. Token length dependent on user input.
+        StringBuilder generatedToken = new StringBuilder();
+
+        switch (userTokenLength) {
+            case 5:
+                System.out.println("User chose token with 5 characters.");
+                for (int i = 0; i < userTokenLength; i++) {
+                    String singleCharacter = tokenDataSetCollection.get(randomNumber.nextInt(tokenDataSetCollection.size()));
+                    generatedToken.append(singleCharacter);
+                }
+                break;
+            case 10:
+                System.out.println("User chose token with 10 characters.");
+                for (int i = 0; i < userTokenLength; i++) {
+                    String singleCharacter = tokenDataSetCollection.get(randomNumber.nextInt(tokenDataSetCollection.size()));
+                    generatedToken.append(singleCharacter);
+                }
+                break;
+            case 15:
+                System.out.println("User chose token with 15 characters.");
+                for (int i = 0; i < userTokenLength; i++) {
+                    String singleCharacter = tokenDataSetCollection.get(randomNumber.nextInt(tokenDataSetCollection.size()));
+                    generatedToken.append(singleCharacter);
+                }
+                break;
+        }
+        String generatedTokenString = generatedToken.toString();
         return generatedTokenString;
     }
 }
+
 
 
 
